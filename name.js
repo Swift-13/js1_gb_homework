bfa5d1883ce79c8ccb3af691116e0db315665197
Swift <kseniyastrizhak13@gmail.com>
2019-10-16 23:27:04 +0300

    решение пятого задания

--- name.js
+++ name.js
@@ -3,4 +3,15 @@ admin = name;
 alert(admin);
 
 var n = 1000+"108";
-alert(n);
+alert(n); //100108
+
+var a = 10;
+var b = 20;
+[a,b]=[b,a];
+alert(a+" "+b);
+
+var c = 5; 
+var d = 3;
+c=[d, d=c][0];
+alert(c+" "+d);
+
