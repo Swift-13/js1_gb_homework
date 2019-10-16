f9f746751e6121904974dca1abec81e08755fda1
Swift <kseniyastrizhak13@gmail.com>
2019-10-16 23:50:51 +0300

    задания первого урока

---
+++ name.js
@@ -0,0 +1,17 @@
+var name = "Василий";
+admin = name;
+alert(admin);
+
+var n = 1000+"108";
+alert(n); //100108
+
+var a = 10;
+var b = 20;
+[a,b]=[b,a];
+alert(a+" "+b);
+
+var c = 5; 
+var d = 3;
+c=[d, d=c][0];
+alert(c+" "+d);
+
